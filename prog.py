0c7616fdb347adf44236f36172ce8cae086b735b
Harikrishnan-Raman18 <Harikrishnan_Raman@comcast.com>
2025-12-19 18:59:20 +0530

    modified:   prog.py

--- prog.py
+++ prog.py
@@ -1,4 +1,11 @@
 
 from datetime import date
+from modulefinder import test
 
 print(date.today())
+
+def test():
+    added_line = "This is a new line added in the function."
+    print(added_line)
+    return added_line
+test()
