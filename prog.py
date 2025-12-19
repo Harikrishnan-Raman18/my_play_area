632f9ebcbfd65a6e4602f5bb79951d2db74cd270
Harikrishnan-Raman18 <58827168+Harikrishnan-Raman18@users.noreply.github.com>
2025-12-19 19:00:05 +0530

    Update prog.py

--- prog.py
+++ prog.py
@@ -5,7 +5,7 @@ from modulefinder import test
 print(date.today())
 
 def test():
-    added_line = "This is a new line added in the function."
-    print(added_line)
+    added_line = "This is a new line added in the function
+    print(added_line
     return added_line
 test()
